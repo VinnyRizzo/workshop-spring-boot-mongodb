f2caf22ce81892f102c45b55bd5e5c48dc23ddc6
VinnyRizzo <viniciusbragarizzo@gmail.com>
2025-08-04 17:22:38 -0300

    Obtendo post por id

---
+++ src/main/java/com/vinnyrizzo/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.vinnyrizzo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vinnyrizzo.workshopmongo.domain.Post;
+import com.vinnyrizzo.workshopmongo.repository.PostRepository;
+import com.vinnyrizzo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	  public Post findById(String id) {
+	        Optional<Post> obj = repo.findById(id);
+	        return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado!"));
+	    }
+
+}
